724f05cb99dae4a9a502d1239a50c9a88dcfd86d
Adrian Prananda <mprananda@liveperson.com>
2021-11-12 15:19:17 -0800

    Update coverage script

--- python/scripts/generate_coverage.sh
+++ python/scripts/generate_coverage.sh
@@ -1,3 +1,3 @@
 set -e
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-py.test SCRIPT_DIR/../tests/ --cov-report xml:SCRIPT_DIR/../cov.xml --cov package
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+py.test $SCRIPT_DIR/../tests/ --cov-report xml:$SCRIPT_DIR/../cov.xml --cov package
