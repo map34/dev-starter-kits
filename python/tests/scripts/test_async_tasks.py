e90aac3da21a7ac7a807edbaca0e2ea5e2a7ed62
Adrian Prananda <prananda0203@gmail.com>
2021-11-16 11:17:05 -0800

    Refactor main

--- python/tests/scripts/test_async_tasks.py
+++ python/tests/scripts/test_async_tasks.py
@@ -1,9 +1,30 @@
 from unittest import IsolatedAsyncioTestCase, mock
 from package.scripts.async_tasks import sum
+from package.scripts.async_tasks import get
 
-class Test(IsolatedAsyncioTestCase):
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse():
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.headers = json_data
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'https://google.com':
+        return MockResponse({"key1": "value1"}, 200)
+
+    return MockResponse(None, 404)
+
+class AsyncTasksTest(IsolatedAsyncioTestCase):
 
     @mock.patch("asyncio.sleep")
     async def test_sum(self, *_):
         result = await sum("A", [1, 2, 3])
         self.assertEqual(result, 6)
+
+    @mock.patch("requests.get", mocked_requests_get)
+    async def test_get(self, *_):
+        result = await get("https://google.com")
+        self.assertEqual(result.status_code, 200)
--- python/tests/test_execute.py
+++ python/tests/test_execute.py
@@ -1,34 +1,13 @@
-from package import execute
+from package import main
 import unittest
-from unittest import mock
-
-
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse():
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-            self.headers = json_data
-
-        def json(self):
-            return self.json_data
-
-    if args[0] == 'https://google.com':
-        return MockResponse({"key1": "value1"}, 200)
-
-    return MockResponse(None, 404)
-
 
 class ExecuteTests(unittest.TestCase):
 
     def setUp(self):
         super().setUp()
 
-    @mock.patch("requests.get", side_effect=mocked_requests_get)
-    @mock.patch("package.scripts.async_tasks.main")
-    def test_main(self, *args):
-        value = execute.main()
-        assert isinstance(value, dict)
+    def test_main(self):
+        main.main()
 
     def tearDown(self):
         super().tearDown()
